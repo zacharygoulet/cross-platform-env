965d970ee7dcf597928cce634b07766b6f037f1b
zacharygoulet <zachary.goulet9@gmail.com>
2023-10-07 15:14:49 -0400

    windows progress

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -4,7 +4,11 @@ irm get.scoop.sh | iex
 
 # Install Packages
 scoop bucket add nerd-fonts
-scoop install alacritty nushell starship nerd-fonts/UbuntuMono-NF
+scoop install git alacritty nushell starship nerd-fonts/UbuntuMono-NF
+
+
+git config --global core.autocrlf true
+# git config --global core.autocrlf input
 
 # Copy Config Files
 mkdir $env:APPDATA/alacritty
