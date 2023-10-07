7ad5a912eea2edd9974794b87758631fc74bdb4d
zacharygoulet <zachary.goulet9@gmail.com>
2023-10-07 12:18:57 -0400

    windows progress

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -3,6 +3,9 @@ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 
 # Install Packages
-scoop install alacritty nushell starship
+scoop bucket add nerd-fonts
+scoop install alacritty nushell starship nerd-fonts/UbuntuMono-NF
 
 # Copy Config Files
+mkdir $env:APPDATA/alacritty
+cp alacritty.yml $env:APPDATA/alacritty
